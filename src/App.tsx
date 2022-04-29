7571fcf26654e006a30db242827ee64bb805f96f
ethan <benidiktusvio@gmail.com>
2022-04-29 15:52:45 +0700

    Update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <button type="button" onClick={() => setCount((count) => count + 1)}>
-        Count: {count}
-      </button>
-    </>
+    <div className="select-none bg-gray-800 flex flex-row items-center justify-center min-h-screen">
+      <main className="bg-gray-600 flex flex-col items-center justify-center w-9/12 pt-4 pb-4">
+        <h2 className="text-6xl text-white pt-6 tracking-wide transition-all hover:scale-110">Hi, I am Vila.</h2>
+
+        <img className="m-6 filter saturate-25 w-48 h-48 ring-4 ring-white transition-all transform opacity-75 hover:scale-110 hover:opacity-100" src="https://raw.githubusercontent.com/vt92i/dvonick.surge.sh/main/assets/avatar.JPG" />
+
+        <div className="flex flex-row pt-2">
+          <a className="pl-4 transition-transform transform hover:scale-125" href="https://github.com/vt92i" target="_blank" rel="noopener noreferrer">
+            <img className="w-8" src="https://raw.githubusercontent.com/vt92i/dvonick.surge.sh/main/assets/github.png" />
+          </a>
+          <a className="pl-4 transition-transform transform hover:scale-125" href="https://twitter.com/wtsrz4" target="_blank" rel="noopener noreferrer">
+            <img className="w-8" src="https://raw.githubusercontent.com/vt92i/dvonick.surge.sh/main/assets/twitter.png" />
+          </a>
+        </div>
+
+        <footer className="flex flex-col items-center pt-4">
+          <span className="text-sm font-light text-white">
+            Hosted on
+            <a className="text-gray-100 font-bold opacity-75 transition-opacity hover:opacity-100" href="https://surge.sh/" target="_blank" rel="noopener noreferrer"> Github </a>
+          </span>
+          <span>❤️</span>
+        </footer>
+      </main>
+    </div>
   )
 }
 
